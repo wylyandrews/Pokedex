414cf7632007126260e3bd5c66d8b015f3fed6c9
sirdude103 <sirdude103@gmail.com>
2019-03-04 12:04:23 -0600

    preliminary create pokemon table

---
+++ database/migrations/create_pokemon_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePokemonTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pokemon', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->decimal('height');
+            $table->decimal('weight');
+            $table->string('genus');
+            $table->string('description');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pokemon');
+    }
+}
